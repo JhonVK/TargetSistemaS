6c30977c65c9bf804561b8d77e3cf4e04efde981
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-09-18 16:39:15 -0300

    update

---
+++ faturamento.c
@@ -0,0 +1,15 @@
+//Joao vitor kauer schuck
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cJSON.h"
+
+int media(int *vetorExemplo, int tamanho){
+
+}
+
+int main() {
+  
+    return 0;
+}
---
+++ fibonacci.c
@@ -0,0 +1,41 @@
+//Joao vitor kauer schuck
+//codigo feito sem nenhum auxilio externo
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool fibonacci(int entrada, int numeroT){
+    int primeiro=0, segundo=1, terceiro;
+    bool saida=false; //na teoria um bool em C ja é inicializado com false, entao o false nao seria necessario aqui
+
+    printf("%d\n", primeiro);
+    printf("%d\n", segundo);
+    if(entrada==primeiro || entrada==segundo){
+        saida=true;
+    }
+    for(int i=numeroT-2; i>0; i--){
+        terceiro=primeiro+segundo;
+        if(terceiro==entrada){
+            saida=true;
+        }
+        primeiro=segundo;
+        segundo=terceiro;
+        printf("%d\n", terceiro);
+    }
+    return saida;
+}
+
+int main() {
+    int entrada, numeroT;
+    printf("Digite quantos numeros da sequencia de fibonacci voce deseja imprimir: ");
+    scanf("%d", &numeroT);
+    printf("Digite o numero que deseja verificar se faz parte da sequencia: ");
+    scanf("%d", &entrada);
+    bool saida=fibonacci(entrada, numeroT);
+    if(saida){
+        printf("O numero %d esta na sequecia!!", entrada);
+    }else{
+        printf("O numero %d nao esta na sequecia!!", entrada);
+    }
+    return 0;
+}
