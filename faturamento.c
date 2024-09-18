569516e260fbed576845b4f0c49e880d53d8d41d
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-09-18 17:05:09 -0300

    update

--- faturamento.c
+++ faturamento.c
@@ -6,9 +6,29 @@
 #include "cJSON_Parser/cJSON.h"
 
 int main() {
-  
+    FILE *file = fopen("arquivoJSON/dados.json", "r");
+    if (file == NULL) {
+        printf("Não foi possível abrir o arquivo.\n");
+        return 1;
+    }
+
+    char memoria[4096];
+    size_t tamanho_arquivo = fread(memoria, 1, sizeof(memoria), file);
+    fclose(file);
+
+    if (tamanho_arquivo == 0) {
+        printf("Arquivo vazio ou erro na leitura.\n");
+        return 1;
+    }
+
+    memoria[tamanho_arquivo] = '\0'; 
+
+    cJSON *json = cJSON_Parse(memoria);
+    char *json_string = cJSON_Print(json);
+    printf("%s\n", json_string);
 
 
-
+    free(json_string);
+    cJSON_Delete(json);
     return 0;
 }
